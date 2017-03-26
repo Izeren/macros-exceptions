d27b2b013db3161363b47c65f781cea0bf039856
Alexander Yushmanov <yushalnik@bk.ru>
2017-03-27 01:26:16 +0300

    Added inheritance test

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 
 class MyException1 : public CException {
 public:
-    MyException1( std::string _message ) : CException( _message ) {};
+    MyException1( std::string _message = "default error 1" ) : CException( _message ) {};
 };
 
 class MyException2 : public CException {
 public:
-    MyException2( std::string _message ) : CException( _message ) {};
+    MyException2( std::string _message = "default error 2" ) : CException( _message ) {};
 };
+
+class MyException3 : public MyException2 {
+public:
+    MyException3( std::string _message = "default error 3" ) : MyException2( _message ) {};
+};
+
 
 int DangerousMethod1() {
     THROW( new MyException1( "Some error of type 1" ));
@@ -25,17 +31,22 @@ int DangerousMethod2() {
 
 int main() {
     TRY
-        DangerousMethod2();
-    CATCH( MyException1, e )
-        std::cout << e->GetMessage() << "\n";
-    CATCH( MyException2, e )
+        THROW( new MyException3())
+    CATCH( MyException3, e )
         std::cout << e->GetMessage() << "\n";
     ETRY
-
-    TRY
-        THROW( new MyException1( "---" ));
-    CATCH( MyException1, e )
-        std::cout << e->GetMessage() << "\n";
-    ETRY
+//    TRY
+//        DangerousMethod2();
+//    CATCH( MyException1, e )
+//        std::cout << e->GetMessage() << "\n";
+//    CATCH( MyException2, e )
+//        std::cout << e->GetMessage() << "\n";
+//    ETRY
+//
+//    TRY
+//        THROW( new MyException1( "---" ));
+//    CATCH( MyException1, e )
+//        std::cout << e->GetMessage() << "\n";
+//    ETRY
     return 0;
 }
